0061881ad3002536ed1f3977bd2545e1386b9a22
Vor <sketchpunk@gmail.com>
2016-07-27 14:06:33 -0400

    Spawning Enemies
    Added a spawn manager and spawn points to control where enemies will appear

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+	#region Variables
+	private SpawnPoint[] mPosition;
+	public GameObject mPrefab;
+	#endregion
+
+	#region Behavior Events
+	public void Start (){
+		mPosition = GameObject.FindObjectsOfType<SpawnPoint>();
+
+		//for(int i = 0; i < mPosition.Length; i++){ SpawnAt(i); }
+		RandomSpawn();
+	}
+	#endregion
+
+	#region Spawn Methods
+	public GameObject SpawnAt(int i){
+		Vector3 pos = mPosition[i].transform.position + mPosition[i].transform.up;
+		Quaternion rot = Quaternion.LookRotation(mPosition[i].transform.up);
+
+		var go = (GameObject)Instantiate(mPrefab,pos,rot);
+		go.transform.parent = this.transform;
+
+		return go;
+	}
+
+	public void RandomSpawn(){
+		int i = Random.Range(0,mPosition.Length);
+		Debug.Log(i);
+		SpawnAt(i);
+	}
+	#endregion
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+	//TODO: Might not need this class, if that becomes true, change the Spawn Manager to find the Spawn points through a tag.
+	//void Start(){}
+	//void Update(){}
+}
